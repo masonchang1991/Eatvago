2bf00edb5bd1d16fb4d34460fcba14f83d1ddd4c
Mason Chang <masonchang1991@gmail.com>
2017-08-01 09:04:00 +0800

    create a branch to fix GCD

---
+++ Eatvago/FetchPlaceImageManager.swift
@@ -0,0 +1,9 @@
+//
+//  FetchPlaceImageManager.swift
+//  Eatvago
+//
+//  Created by Ｍason Chang on 2017/7/31.
+//  Copyright © 2017年 Ｍason Chang iOS#4. All rights reserved.
+//
+
+import Foundation
