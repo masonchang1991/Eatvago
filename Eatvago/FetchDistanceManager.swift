7fd31ec05d3fc697ba88642432e39a471558723c
Mason Chang <masonchang1991@gmail.com>
2017-08-13 11:24:57 +0800

    no change

--- Eatvago/FetchDistanceManager.swift
+++ Eatvago/FetchDistanceManager.swift
@@ -25,6 +25,15 @@ class FetchDistanceManager {
         var fetchCount = 0
         locations = []
         
+        ///google 壞掉 特地加這行
+        
+         self.delegate?.manager(self, didGet: nearLocations)
+        
+        return
+        
+        
+        
+        
         for nearLocation in nearLocations {
             
             let urlStringForDistance = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=\(myLocation.coordinate.latitude),\(myLocation.coordinate.longitude)&destinations=\(nearLocation.latitude),\(nearLocation.longitude)&key=\(googleMapDistanceMatrixAPIKey)"
